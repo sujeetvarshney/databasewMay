b6b3e0a9c30884badedef68dee8f0c5307d34a0e
sujeetvarshney <74142145+sujeetvarshney@users.noreply.github.com>
2024-05-08 17:05:17 +0530

    day 1 commands

---
+++ day1.sql
@@ -0,0 +1,10 @@
+-- create database command is used to create databases in mysql 
+-- syntax of creating database is 
+-- create database <database name>;
+
+
+CREATE DATABASE XYZSCHOOL;
+CREATE DATABASE HDFC_KRISHNA_NAGAR1;
+
+
+DROP DATABASE hdfc_krishna_nagar2;
